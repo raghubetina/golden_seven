8eea116be8e115b8567fe2f300b9bb17863b3cad
Raghu Betina <rbetina@gmail.com>
2013-07-22 15:10:09 -0500

    Next up: the user should be able to delete a picture.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ GoldenSeven::Application.routes.draw do
 
   get "/pictures/:id/edit" => "pictures#edit"
   patch "/pictures/:id" => "pictures#update"
+
+  delete "/pictures:id" => "pictures#destroy"
 end
